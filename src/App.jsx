3503222d265a76caaabdb20342d2d1b975b69aa5
Daniel Javier Pulla S <daniel.javier.pulla@yandex.com>
2023-01-31 18:53:48 -0500

    style(App): add support of tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-
 import TaskList from "./components/task/TaskList";
 import TaskForm from "./components/task/TaskForm";
 
 function App() {
-
 	return (
-		<>
-			<TaskForm />
-			<TaskList />
-		</>
+		<main className="bg-zinc-900 h-screen">
+			<div className="container mx-auto p-10">
+				<TaskForm />
+				<TaskList />
+			</div>
+		</main>
 	);
 }
 
--- src/components/task/TaskForm.jsx
+++ src/components/task/TaskForm.jsx
@@ -17,23 +17,28 @@ export default function TaskForm() {
 	};
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<input
-				placeholder="Escribe tu tarea"
-				onChange={(e) => setTitle(e.target.value)}
-				value={title}
-				autoFocus
-			></input>
-			<textarea
-				name="taskDesc"
-				id="taskDesc"
-				cols="30"
-				rows="5"
-				placeholder="Escribe la descripción de la tarea..."
-				onChange={(e) => setDescription(e.target.value)}
-				value={description}
-			></textarea>
-			<button>Guardar</button>
-		</form>
+		<div className="max-w-md mx-auto">
+			<form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4">
+				<h1 className="text-2xl text-white font-bold mb-3">Crea tu tarea</h1>
+				<input
+					placeholder="Escribe tu tarea"
+					onChange={(e) => setTitle(e.target.value)}
+					value={title}
+					className="bg-slate-300 p-3 w-full mb-2"
+					autoFocus
+				></input>
+				<textarea
+					name="taskDesc"
+					id="taskDesc"
+					cols="30"
+					rows="5"
+					placeholder="Escribe la descripción de la tarea..."
+					onChange={(e) => setDescription(e.target.value)}
+					value={description}
+					className="bg-slate-300 p-3 w-full mb-2"
+				></textarea>
+				<button className="bg-indigo-500 px-3 py-1 text-white rounded-md hover:bg-indigo-400">Guardar</button>
+			</form>
+		</div>
 	);
 }
