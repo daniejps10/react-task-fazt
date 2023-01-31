716e87433c706bafdd672423c9e6da31410fe0b5
Daniel Javier Pulla S <daniel.javier.pulla@yandex.com>
2023-01-31 11:15:10 -0500

    feat(TaskList): add support to list tasks

---
+++ src/tasks.js
@@ -0,0 +1,22 @@
+export const tasks = [
+   {
+      id: 0,
+      title: 'Mi primer tarea',
+      description: 'Esta es mi primer tarea'
+   },
+   {
+      id: 1,
+      title: 'Mi segunda tarea',
+      description: 'Esta es mi segunda tarea'
+   },
+   {
+      id: 2,
+      title: 'Mi tercera tarea',
+      description: 'Esta es mi tercera tarea'
+   },
+   {
+      id: 3,
+      title: 'Mi cuarta tarea',
+      description: 'Esta es mi cuarta tarea'
+   },
+]
